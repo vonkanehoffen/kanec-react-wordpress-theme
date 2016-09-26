a66e4e21b6941c3bd82fb2ef3d5817af7cac498d
Kane Clover <kane@assure24.com>
2016-09-26 20:26:53 +0100

    Move demo

--- client/main.jsx
+++ client/main.jsx
@@ -5,8 +5,8 @@ import 'styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './app/store';
-import router from './app/router';
+import store from './app_css-tricks-demo/store';
+import router from './app_css-tricks-demo/router';
 
 // Provider is a top-level component that wrapps our entire application, including
 // the Router. We pass it a reference to the store so we can use react-redux's
