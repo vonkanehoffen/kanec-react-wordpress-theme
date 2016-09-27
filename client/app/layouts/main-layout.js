40de2d5052bf75f7370b1a9a558f2be4fa6e17b7
Kane Clover <kane@assure24.com>
2016-09-27 17:49:39 +0100

    Material UI toolbar

--- client/app/layouts/main-layout.js
+++ client/app/layouts/main-layout.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 export default function(props) {
   return (
     <div className="app">
-      <h1>Main Layout</h1>
+      <Toolbar>
+        <ToolbarGroup>
+          <ToolbarTitle text="Bookmark Thing" />
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <RaisedButton label="Refresh Bookmarks" secondary={true} />
+        </ToolbarGroup>
+      </Toolbar>
       <main>
         {props.children}
       </main>
