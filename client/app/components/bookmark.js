8c0e6ca4a3f640d187c5fabcac364228eb65b9b5
Kane Clover <kane@assure24.com>
2016-09-27 15:11:43 +0100

    Use a material-ui component for the bookmarks

--- client/app/components/bookmark.js
+++ client/app/components/bookmark.js
@@ -1,9 +1,13 @@
 import React, {PropTypes} from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
 
 const Bookmark = (props) => {
   return (
     <div className="Bookmark">
-      <a href={props.url}>{props.title}</a>
+      <RaisedButton
+        label={props.title}
+        href={props.url}
+        primary={true} />
     </div>
   );
 }
--- client/main.jsx
+++ client/main.jsx
@@ -4,14 +4,23 @@ import 'styles/main.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import router from './app/router';
 
+// Temporary fix for material-ui apparently.
+// https://www.npmjs.com/package/material-ui#react-tap-event-plugin
+injectTapEventPlugin();
+
 // Provider is a top-level component that wrapps our entire application, including
 // the Router. We pass it a reference to the store so we can use react-redux's
 // connect() method for Component Containers.
+
 ReactDOM.render(
-  <Provider store={store}>{router}</Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>{router}</Provider>
+  </MuiThemeProvider>,
   document.getElementById('js-main')
 );
