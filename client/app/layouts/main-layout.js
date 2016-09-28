e66636f9d37c8dc19e812f0fc2a7920263530136
Kane Clover <kane@assure24.com>
2016-09-28 16:04:20 +0100

    Click action for fetching bookmarks

--- client/app/layouts/main-layout.js
+++ client/app/layouts/main-layout.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import Header from '../containers/header';
 
 export default function(props) {
   return (
     <div className="app">
-      <Toolbar>
-        <ToolbarGroup>
-          <ToolbarTitle text="Bookmark Thing" />
-        </ToolbarGroup>
-        <ToolbarGroup>
-          <RaisedButton label="Refresh Bookmarks" secondary={true} />
-        </ToolbarGroup>
-      </Toolbar>
+      <Header />
       <main>
         {props.children}
       </main>
