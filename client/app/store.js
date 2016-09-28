5b65dd166bfd198219d290eae4368d00d046bc47
Kane Clover <kane@assure24.com>
2016-09-28 15:47:48 +0100

    Add redux-logger

--- client/app/store.js
+++ client/app/store.js
@@ -1,11 +1,14 @@
 import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 
+const logger = createLogger();
 const store = createStore(
   reducers,
   applyMiddleware(
-    thunkMiddleware
+    thunkMiddleware,
+    logger
   )
 );
 export default store;
