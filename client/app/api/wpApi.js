c149f4b7a7669538a75d4b14e4f98078f1d5d868
Kane Clover <kane@assure24.com>
2016-10-14 20:28:39 +0100

    switch to axios

--- client/app/api/wpApi.js
+++ client/app/api/wpApi.js
@@ -1,8 +1,7 @@
+import axios from 'axios';
 const apiRoot = 'http://localhost:1337/kanec.co.uk/wp-json/wp/v2/';
 
-export function get(query) {
-    return fetch(apiRoot + query)
-        .then(function(response) {
-            return response.json();
-        });
+export function get(url, params = []) {
+    return axios.get(apiRoot + url)
+        .then(response => response.data)
 }
